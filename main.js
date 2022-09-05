13b5c5eb1a1efbaf26fd0f4d0b1f3ba8a079531f
khushal ingle <khushalingle151@gmail.com>
2022-09-05 16:39:47 +0530

    updated : error msgs

--- main.js
+++ main.js
@@ -3,28 +3,35 @@ const luckyNumber = document.querySelector("#lucky-number");
 const checkNumber = document.querySelector("#check-number");
 const outputBox = document.querySelector("#output-box");
 
-function calculateSum(Birthdate){
-    Birthdate = Birthdate.replaceAll("-","");
+function calculateSum(Birthdate) {
+    Birthdate = Birthdate.replaceAll("-", "");
     let sum = 0;
-    for(let i = 0; i < Birthdate.length; i++){
+    for (let i = 0; i < Birthdate.length; i++) {
         sum = sum + Number(Birthdate.charAt(i));
-    } 
+    }
     return sum;
 }
 
-function compareValues(sum,luckyNumber){
-   if(sum%luckyNumber === 0){
-    outputBox.innerText = "Your Birthday is Lucky."
-   }else{
-    outputBox.innerText = "Your Birthday is not so lucky.";
-   }
+function compareValues(sum, luckyNumber) {
+    if (sum % luckyNumber === 0) {
+        outputBox.innerText = "Your Birthday is Lucky.";
+    } else {
+        outputBox.innerText = "Your Birthday is not so lucky.";
+    }
 }
 
-checkNumber.addEventListener("click", function checkBirthdayIsLucky(){
-    const Birthdate = dateOfBirth.value;
-     const sum = calculateSum(Birthdate);
-     if(sum && Birthdate)
-     compareValues(sum,luckyNumber.value);
-     else 
-     outputBox.innerText = "Please enter both the fields."
+checkNumber.addEventListener("click", function checkBirthdayIsLucky() {
+    if (dateOfBirth.value === "" || luckyNumber.value === "") {
+        outputBox.innerText = "Please enter values in the fields.";
+    } else if (Number(luckyNumber.value) < 0) {
+        outputBox.innerText = "Please enter values positive values in fields.";
+    } else {
+        const Birthdate = dateOfBirth.value;
+        const sum = calculateSum(Birthdate);
+        if (sum && Birthdate) {
+            compareValues(sum, luckyNumber.value);
+        } else {
+            outputBox.innerText = "Please enter both the fields."
+        }
+    }
 });
